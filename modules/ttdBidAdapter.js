531e8e9aa2a09d3dac69a5c714ed011c35346ba3
Khang Vu <khang.vu@thetradedesk.com>
2025-07-14 16:55:28 -0700

    update ttd version

--- modules/ttdBidAdapter.js
+++ modules/ttdBidAdapter.js
@@ -14,7 +14,7 @@ import { getConnectionType } from '../libraries/connectionInfo/connectionUtils.j
  * @typedef {import('../src/adapters/bidderFactory.js').UserSync} UserSync
  */
 
-const BIDADAPTERVERSION = 'TTD-PREBID-2025.04.25';
+const BIDADAPTERVERSION = 'TTD-PREBID-2025.07.14';
 const BIDDER_CODE = 'ttd';
 const BIDDER_CODE_LONG = 'thetradedesk';
 const BIDDER_ENDPOINT = 'https://direct.adsrvr.org/bid/bidder/';
